f88fc15e7517cc838f45517e1499cafdadc84c11
Monya <seemslikeweonlygobackwards@gmail.com>
2026-04-29 00:08:09 +0400

    Add files via upload

--- medium_sort.c
+++ medium_sort.c
@@ -6,7 +6,7 @@
 /*   By: mavanesy <mavanesy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 20:55:38 by mavanesy          #+#    #+#             */
-/*   Updated: 2026/04/28 23:22:37 by mavanesy         ###   ########.fr       */
+/*   Updated: 2026/04/29 00:07:01 by mavanesy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,25 @@ int	chunk_up(int size)
 	return (chunks);
 }
 
-void	(push_chunks)
+void	push_chunks(t_number *a, t_number *b, int size1, int size2)
+{
+	int	chunks;
+	int	current;
+	int	i;
+
+	i = 0;
+	chunks = chunk_up(size1);
+	current = 1;
+	while (current < chunks)
+	{
+		if (a[i].index <= current * 20)
+			pb(a, b);
+		else
+			ra(a);
+		i++;
+	}
+	current++;
+}
 
 void	medium_sort(t_number *a, t_number *b, int *size1, int *size2)
 {
